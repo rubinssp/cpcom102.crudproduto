e2e3631b32192bb440986d851937955330b7b794
ASUS <ASUS@DESKTOP-BF7N1HC>
2021-11-11 21:29:14 -0200

    Autalização

--- src/model/dao/ProdutoDAO1.java
+++ src/model/dao/ProdutoDAO1.java
@@ -23,21 +23,28 @@ public class ProdutoDAO1 {
         Connection con = ConnectionFactory.getConnection();
          Statement stmt = null;
         
+            
+        try{
             String sql1 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste1',15,5.55)";
             String sql2 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste2',25,5.55)";
             String sql3 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste3',35,5.55)";
-        try{
-            
-            
+            con.setAutoCommit(false);
             stmt = con.createStatement();
             
             stmt.executeUpdate(sql1);
             stmt.executeUpdate(sql2);
+            if(true){
+                throw new SQLException();
+            }
             stmt.executeUpdate(sql3);
-  
+            con.commit();
         }catch(SQLException ex){
            Logger.getLogger(ProdutoDAO1.class.getName()).log(Level.SEVERE, null, ex);
-          
+          try{
+              con.rollback();
+          } catch (SQLException ex1){
+              System.out.println("Erro ao Salvar!");
+          }
         }
     }
 }
