3f33c18820e31da7904f1b5f88144566774bbe71
ASUS <ASUS@DESKTOP-BF7N1HC>
2021-11-11 21:15:43 -0200

    Autalização

--- src/model/dao/ProdutoDAO1.java
+++ src/model/dao/ProdutoDAO1.java
@@ -5,6 +5,14 @@
  */
 package model.dao;
 
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author ASUS
@@ -13,32 +21,23 @@ public class ProdutoDAO1 {
     public void create(){
         
         Connection con = ConnectionFactory.getConnection();
-        Statement stmt = null;
+         Statement stmt = null;
         
-        try{
             String sql1 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste1',15,5.55)";
-            String sql2 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste1',25,5.55)";
-            String sql3 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste1',35,5.55)";
+            String sql2 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste2',25,5.55)";
+            String sql3 = "INSERT INTO produto (descricao, qtd, preco) VALUES ('teste3',35,5.55)";
+        try{
             
-            con.setAutoCommit(false);//interrompe o autocomit que vem true por defaul
+            
             stmt = con.createStatement();
             
             stmt.executeUpdate(sql1);
             stmt.executeUpdate(sql2);
-            if(true){
-                throw new SQLException();
-            }
             stmt.executeUpdate(sql3);
-            
-            con.commit(); // executa a atualização do bd
-            
+  
         }catch(SQLException ex){
            Logger.getLogger(ProdutoDAO1.class.getName()).log(Level.SEVERE, null, ex);
-           try{
-               con.rollback();
-           }catch(SQLException ex1){
-               System.out.println("Erro ao salvar!!");
-           }
+          
         }
     }
 }
